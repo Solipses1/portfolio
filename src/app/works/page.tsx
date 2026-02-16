336a1a799043f6cd9e51f8362d88d6c6374eae13
Jorden <knock007@alumni.umn.edu>
2026-02-16 12:49:08 -0600

    potfolio update

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,10 +1,10 @@
 import ProjectCard from "@/components/project-card";
 
 export default function WorksPage() {
-    return <div className="flex flex-1 justify-center items-center gap-x-8 xl:gap-x-24">
-            <ProjectCard title="Muzario" link="https://play.google.com/store/apps/details?id=com.solipses.muzario" source="https://play.google.com/store/apps/details?id=com.solipses.muzario" image="/hometownhappenings.png" />
-            <ProjectCard title="Hometown Happenings" link="https://hometown-happenings.vercel.app" source="https://github.com/Solipses1/hometown-happenings" image="/hometownhappenings.png" />
-            <ProjectCard title="Artifactle" link="https://artifactle.vercel.app" source="https://github.com/Solipses1/artifactle" image="/artifactle.png" />
-            <ProjectCard title="Artifact Scraper" link="https://github.com/Solipses1/ArtifactScraper" source="https://github.com/Solipses1/ArtifactScraper" image="/artifactscraper.png" />
+    return <div className="flex flex-1 justify-center items-center gap-x-8 xl:gap-x-24 px-5">
+            <ProjectCard title="Muzario" link="https://play.google.com/store/apps/details?id=com.solipses.muzario" source="none" image="/muzarioapp.png" />
+            <ProjectCard title="Hometown Happenings" link="https://hometown-happenings.vercel.app" source="none" image="/hometownhappenings.png" />
+            <ProjectCard title="Artifactle" link="https://artifactle.vercel.app" source="none" image="/artifactle.png" />
+            <ProjectCard title="Artifact Scraper" link="https://github.com/Solipses1/ArtifactScraper" source="none" image="/artifactscraper.png" />
         </div>
 }
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -14,7 +14,37 @@ export default function ProjectCard({
   image,
   source,
 }: ProjectCardProps) {
-  return (
+  if (source != "none") {
+    return (
+        <div className="flex flex-col ">
+          <Link
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-1 basis-60 h-[350px] max-w-[350px] xl:h-[400px] xl:max-w-[400px]"
+          >
+            <div className="flex flex-col w-full h-full text-2xl xl:text-3xl rounded-lg">
+              <Image
+                src={image}
+                alt=""
+                width={350}
+                height={400}
+                className="object-cover rounded-lg"
+              ></Image>
+              <div className="text-yellow-300 text-center mt-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                {title}
+              </div>
+            </div>
+          </Link>
+          <Link href={source} target="_blank" rel="noopener noreferrer">
+            <div className="text-yellow-300 text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+              source code
+            </div>
+          </Link>
+        </div>
+      );
+  } else {
+    return (
     <div className="flex flex-col">
       <Link
         href={link}
@@ -35,11 +65,8 @@ export default function ProjectCard({
           </div>
         </div>
       </Link>
-      <Link href={source} target="_blank" rel="noopener noreferrer">
-        <div className="text-yellow-300 text-center mt-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-          source code
-        </div>
-      </Link>
     </div>
   );
+  }
+  
 }
